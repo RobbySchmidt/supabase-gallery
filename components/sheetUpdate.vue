cf19ec405a20bb59ac280400918843aad50135e6
RobbySchmidt <168719558+RobbySchmidt@users.noreply.github.com>
2025-05-07 08:47:45 +0200

    removed spellcheck from inputs and textareas

--- components/sheetUpdate.vue
+++ components/sheetUpdate.vue
@@ -11,8 +11,8 @@
           <SheetTitle>Upload Image</SheetTitle>
         </SheetHeader>
         <div class="space-x-2 space-y-2">
-          <Input v-model="title" placeholder="Title"/>
-          <Textarea rows="1" v-model="description" placeholder="Description"/>
+          <Input spellcheck="false" v-model="title" placeholder="Title"/>
+          <Textarea spellcheck="false" rows="1" v-model="description" placeholder="Description"/>
           <Button 
             @click="updateFile(image.id, title, description)" 
             class="w-fit hover:cursor-pointer">
